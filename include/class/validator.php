eefcdc822d2297f289d5a17dc4d7e9abc953a996
G-Almighty <60286901+G-Almighty@users.noreply.github.com>
2021-01-08 21:54:05 +0200

    changed some hash

--- include/class/validator.php
+++ include/class/validator.php
@@ -14,7 +14,7 @@ function validator($context)
 {
     global $CURUSER;
     $timestamp = time();
-    $hash = hash_hmac("sha1", $CURUSER['secret'], $context . $timestamp);
+    $hash = hash_hmac("ripemd160", $CURUSER['secret'], $context . $timestamp);
     return substr($hash, 0, 20) . dechex($timestamp);
 }
 function validatorForm($context)
@@ -26,7 +26,7 @@ function validate($validator, $context, $seconds = 0)
     global $CURUSER;
     $timestamp = hexdec(substr($validator, 20));
     if ($seconds && time() > $timestamp + $seconds) return False;
-    $hash = substr(hash_hmac("sha1", $CURUSER['secret'], $context . $timestamp) , 0, 20);
+    $hash = substr(hash_hmac("ripemd160", $CURUSER['secret'], $context . $timestamp) , 0, 20);
     if (substr($validator, 0, 20) != $hash) return False;
     return True;
 }
